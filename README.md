2b6ca3fa7b4438259e45b1295c88edf88c7f6206
Valere Youbi <v.youbi@aftech-solution.com>
2022-12-08 12:30:53 +0100

    first develop commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# hc-service-registry
---
+++ src/test/java/de/haegerconsulting/internShop/hcserviceregistry/HcServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.haegerconsulting.internShop.hcserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
